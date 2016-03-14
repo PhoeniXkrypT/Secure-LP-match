e77fd832c17b2d3d4d9f19bf89badb1ea9956f0e
archanabs <archanabs@users.noreply.github.com>
2016-03-14 15:31:55 +0100

    edit in cpp filename

--- runGentry.sh
+++ runGentry.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -O3 -I$HOME/sw/include gentryextended.cpp gentryprivkeys.cpp gentrypubkeys.cpp test.cpp -o test_gentry -L$HOME/sw/lib -lntl -lgmp -lm
+g++ -O3 -I$HOME/sw/include gentryextended.cpp gentryprivkeys.cpp gentrypubkeys.cpp testGentry.cpp -o test_gentry -L$HOME/sw/lib -lntl -lgmp -lm
